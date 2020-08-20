db1c8da9cc6ed17b44e13aebe4eaefa61d4a16af
Theodoros Demertzis <tdr.demertzis94@hotmail.com>
2020-08-20 19:56:40 +0300

    zeroed concaveman last parameter

--- src/concaveCross.js
+++ src/concaveCross.js
@@ -3,11 +3,7 @@ import pathTransform from './pathTransform.js';
 import concaveman from 'concaveman';
 
 export default function concaveCross(polyline1, polyline2, offset) {
-  let polygone1 = polygon([
-    concaveman(pathTransform(polyline1, offset), 2, 0.03),
-  ]);
-  let polygone2 = polygon([
-    concaveman(pathTransform(polyline2, offset), 2, 0.03),
-  ]);
+  let polygone1 = polygon([concaveman(pathTransform(polyline1, offset), 2, 0)]);
+  let polygone2 = polygon([concaveman(pathTransform(polyline2, offset), 2, 0)]);
   return !booleanDisjoint(polygone1, polygone2);
 }
