d821f7ff0d52eebe2bb7d912bfc70ce176f4ccb4
Theodoros Demertzis <tdr.demertzis94@hotmail.com>
2020-08-21 18:26:05 +0300

    cleaned up some more

--- src/concaveCross.js
+++
@@ -1,9 +0,0 @@
-import { polygon, booleanDisjoint } from '@turf/turf/turf.js';
-import pathTransform from './pathTransform.js';
-import concaveman from 'concaveman';
-
-export default function concaveCross(polyline1, polyline2, offset) {
-  let polygone1 = polygon([concaveman(pathTransform(polyline1, offset), 2, 0)]);
-  let polygone2 = polygon([concaveman(pathTransform(polyline2, offset), 2, 0)]);
-  return !booleanDisjoint(polygone1, polygone2);
-}
